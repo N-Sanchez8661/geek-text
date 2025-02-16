f81f5f9b048885f2c4b14d54737cfaae29dd0730
rserralta <116393368+rserralta@users.noreply.github.com>
2025-02-16 12:43:11 -0500

    Update ShoppingCartController.java

--- src/main/java/com/example/shopping_cart_api/controller/ShoppingCartController.java
+++ src/main/java/com/example/shopping_cart_api/controller/ShoppingCartController.java
@@ -1,16 +1,10 @@
 package com.example.shopping_cart_api.controller;
 
-import com.example.shopping_cart_api.model.Book;
-import com.example.shopping_cart_api.model.OrderItem;
 import com.example.shopping_cart_api.model.ShoppingCart;
-import com.example.shopping_cart_api.repository.OrderItemRepository;
 import com.example.shopping_cart_api.repository.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/shopping-cart")
@@ -19,43 +13,19 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
-    @Autowired
-    private OrderItemRepository orderItemRepository;
-
-    @GetMapping("/items/{user_id}")
-    public ResponseEntity<List<Book>> getCartItems(@PathVariable Long user_id) {
-        ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(user_id);
-        if (shoppingCart == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        List<OrderItem> orderItems = orderItemRepository.findByShoppingCart_CartId(shoppingCart.getCartId());
-        List<Book> books = new ArrayList<>();
-
-        if (orderItems != null) {
-            for(OrderItem orderItem : orderItems) {
-                books.add(orderItem.getBook());
-            }
-        }
-
-        return ResponseEntity.ok(books);
-    }
-
     @GetMapping("/subtotal/{user_id}")
     public ResponseEntity<Double> getSubtotal(@PathVariable Long user_id) {
         ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(user_id);
+
         if (shoppingCart == null) {
             return ResponseEntity.notFound().build();
         }
 
-        List<OrderItem> orderItems = orderItemRepository.findByShoppingCart_CartId(shoppingCart.getCartId());
-
-        double subtotal = 0.0;
-        if (orderItems != null) {
-            for (OrderItem orderItem : orderItems) {
-                subtotal += orderItem.getBook().getPrice() * orderItem.getQuantity();
-            }
+        if (shoppingCart.getBook() == null) {
+            return ResponseEntity.ok(0.0); /
         }
+
+        double subtotal = shoppingCart.getBook().getPrice(); 
 
         return ResponseEntity.ok(subtotal);
     }

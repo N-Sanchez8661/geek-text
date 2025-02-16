9f56ce79af0e9b2947a91d866d2bf082f62d7a34
rserralta <116393368+rserralta@users.noreply.github.com>
2025-02-16 12:50:01 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (user_id, first_name, last_name, age, occupation) 
-VALUES (1, 'John', 'Doe', 30, 'Software Engineer');
+INSERT INTO users (user_id) 
+VALUES (1);
 
 INSERT INTO books (book_id, title, price) 
 VALUES (1, 'The Hitchhiker''s Guide to the Galaxy', 7.99);

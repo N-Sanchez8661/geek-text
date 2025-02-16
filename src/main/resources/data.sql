7d36ff7fb63695df98782043415cc5f072b3c533
rserralta <116393368+rserralta@users.noreply.github.com>
2025-02-16 12:48:57 -0500

    Create data.sql
    ima have to change this

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (user_id, first_name, last_name, age, occupation) 
+VALUES (1, 'John', 'Doe', 30, 'Software Engineer');
+
+INSERT INTO books (book_id, title, price) 
+VALUES (1, 'The Hitchhiker''s Guide to the Galaxy', 7.99);
+
+INSERT INTO shopping_cart (cart_id, user_id, book_id) 
+VALUES (1, 1, 1);
+

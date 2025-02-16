a9e3bdc42433cf8e6c4596062c0ccf0f9c1d2df3
rserralta <116393368+rserralta@users.noreply.github.com>
2025-02-16 11:41:48 -0500

    Update shopping_cart.sql

--- src/main/resources/shopping_cart.sql
+++ src/main/resources/shopping_cart.sql
@@ -1,21 +1,19 @@
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY
+  
 );
 
 CREATE TABLE books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    price DOUBLE,
-    author_id INT,  
-    publisher_id INT 
+    price DOUBLE
+
 );
 
 CREATE TABLE shopping_cart (
     cart_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    subtotal DOUBLE,
-    book_id INT,  
+    book_id INT, 
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
 );
-

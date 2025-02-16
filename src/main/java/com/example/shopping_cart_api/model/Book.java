762f61e065281571427bf6442d824af20cde5140
rserralta <116393368+rserralta@users.noreply.github.com>
2025-02-16 11:58:20 -0500

    Update Book.java

---
+++ src/main/java/com/example/shopping_cart_api/model/Book.java
@@ -0,0 +1,43 @@
+package com.example.shopping_cart_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private double price;
+
+    public Long getBookId() {
+        return bookId;
+    }
+  
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+  
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

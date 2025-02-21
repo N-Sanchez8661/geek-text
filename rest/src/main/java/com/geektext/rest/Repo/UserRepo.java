be9895317c678e8700eef164ba5c52e7f213d119
Nicole Sanchez <nicolesanchez8661@gmail.com>
2025-02-21 18:06:20 -0500

    delete duplicate files

--- rest/src/main/java/com/geektext/rest/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.geektext.rest.Repo;
-
-import com.geektext.rest.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepo extends JpaRepository<User, Long>{
-
-}
--- rest/src/main/java/com/geektext/rest/Repo/WishlistRepo.java
+++
@@ -1,7 +0,0 @@
-package com.geektext.rest.Repo;
-
-import com.geektext.rest.Model.Wishlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WishlistRepo extends JpaRepository<Wishlist, Long> {
-}

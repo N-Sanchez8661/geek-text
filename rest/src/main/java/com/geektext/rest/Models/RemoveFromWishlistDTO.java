cb20db5218e73a2d848bdb656104db686eb4e965
Nicole Sanchez <nicolesanchez8661@gmail.com>
2025-04-13 16:57:41 -0400

    delete problem files

--- rest/src/main/java/com/geektext/rest/Models/RemoveFromWishlistDTO.java
+++
@@ -1,7 +0,0 @@
-package com.geektext.rest.Models;
-
-public class RemoveFromWishlistDTO {
-    public Long wishlistId;
-    public Long bookId;
-    public Long userId;
-}
--- rest/src/main/java/com/geektext/rest/Models/WishlistBookDTO.java
+++
@@ -1,6 +0,0 @@
-package com.geektext.rest.Models;
-
-public class WishlistBookDTO {
-    public Long wishlistId;
-    public Long bookId;
-}
